20356f900c5d49b22d1fc33e5ddd9296ec08a431
yaroslav-kyrychenko <yaro.kyrychenko@gmail.com>
2023-10-26 20:13:27 +0200

    Debug and optimising

--- js/main.js
+++ js/main.js
@@ -13,10 +13,8 @@ const inputResumeMapping = {
   'input-social-media-links': 'resume-social-media-links',
   // WRÓĆ POTRZEBNE JEST OBEJŚCIE TAM GDZIE MA BYĆ KILKA TAKICH SAMYCH ELEMENTÓW
   'input-university-name': 'resume-university-name',
-  // WRÓĆ DO NAPRAWY TO, ŻE ZMIANY REAGUJĄ TYLKO NA KLAWIATURĘ
   'input-education-level': 'resume-education-level',
   'input-degree-name': 'resume-degree-name',
-  // WRÓĆ DO NAPRAWY TO ŻE NIE WYŚWIETLA SIĘ SŁOWO SPECJALNOŚĆ
   'input-specialisation-name': 'resume-specialisation-name',
   'input-degree-start-year': 'resume-degree-start-year',
   'input-degree-end-year': 'resume-degree-end-year',
@@ -38,7 +36,7 @@ btnListExpand.forEach((btnExpand) => {
   });
 });
 
-// WRÓĆ DO PRZEROBIENIA NA NOWY ALGORYTM
+// WRÓĆ DO PRZEROBIEN3IA NA NOWY ALGORYTM
 inputName.addEventListener('focusin', () => {
   resumeName.classList.add('active-resume-element');
 });
@@ -47,30 +45,50 @@ inputName.addEventListener('focusout', () => {
   resumeName.classList.remove('active-resume-element');
 });
 
-// WRÓĆ DO USUNIĘCIA
-inputName.addEventListener('keydown', (e) => {
-  if (
-    (e.key.length === 1 || e.key === 'Backspace') &&
-    inputName.value.length > 0
-  ) {
-    setTimeout(() => {
-      resumeName.textContent = inputName.value;
-    }, 100);
-  }
-});
+const listenForChangeInResumeText = function (
+  inputHTMLElement,
+  resumeHTMLElement
+) {
+  inputHTMLElement.addEventListener('input', () =>
+    debouncedUpdateResumeTextHandler(inputHTMLElement, resumeHTMLElement)
+  );
 
-const setResumeTextContent = function (inputHTMLElement, resumeHTMLElement) {
-  inputHTMLElement.addEventListener('keydown', (e) => {
-    if (
-      (e.key.length === 1 || e.key === 'Backspace') &&
-      inputHTMLElement.value.length > 0
-    ) {
-      setTimeout(() => {
-        resumeHTMLElement.textContent = inputHTMLElement.value;
-        console.log(resumeHTMLElement.textContent);
-      }, 100);
-    }
+  inputHTMLElement.addEventListener('change', () => {
+    updateResumeText(inputHTMLElement, resumeHTMLElement);
   });
+};
+
+const debounce = function (functionDebounced, delayInMilliseconds) {
+  let timerId;
+  return function (...args) {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(() => {
+      functionDebounced(...args);
+      timerId = null;
+    }, delayInMilliseconds);
+  };
+};
+
+const updateResumeText = function (inputHTMLElement, resumeHTMLElement) {
+  if (inputHTMLElement.getAttribute('type') === 'month') {
+    resumeHTMLElement.textContent = getFormattedDate(inputHTMLElement.value);
+  } else if (
+    resumeHTMLElement.classList.contains('resume-specialisation-name')
+  ) {
+    resumeHTMLElement.textContent = `Specjalność: ${inputHTMLElement.value}`;
+  } else {
+    resumeHTMLElement.textContent = inputHTMLElement.value;
+  }
+};
+
+const debouncedUpdateResumeTextHandler = debounce(updateResumeText, 100);
+
+const getFormattedDate = function (stringDate) {
+  const [year, month] = stringDate.split('-');
+  const formattedDate = `${month}/${year}`;
+  return formattedDate;
 };
 
 const listInputElements = document.querySelectorAll('.input-element');
@@ -82,7 +100,7 @@ listInputElements.forEach((inputElement) => {
       const resumeHTMLElement = document.querySelector(
         `.${inputResumeMapping[inputItem]}`
       );
-      setResumeTextContent(inputHTMLElement, resumeHTMLElement);
+      listenForChangeInResumeText(inputHTMLElement, resumeHTMLElement);
       break;
     }
   }
